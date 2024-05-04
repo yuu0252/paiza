03b83fdb2acde3b8dc2af549a837bbe8e6418808
yuu0252 <yutanmaron@gmail.com>
2024-05-05 08:30:03 +0900

    progressing

--- グラフ・DFSメニュー/6/main.py
+++ グラフ・DFSメニュー/6/main.py
@@ -1,8 +1,26 @@
+from collections import deque
+
 N, S, T = map(int, input().split())
 K = int(input())
-S = list(map(int, input().split()))
+S_list = list(map(int, input().split()))
+
+points = {}
 
 for _ in range(N):
   n = int(input())
   a = list(map(int, input().split()))
-  
+  points[n] = a
+
+q = deque()
+q.append((S, [S]))
+
+# while q:
+#   now, path = q.popleft()
+#   if now == T:
+#     print(*path)
+#   for i in points[now]:
+#     if i not in path:
+#       q.append((i, [*path, i]))
+
+print(S)
+print(points)
